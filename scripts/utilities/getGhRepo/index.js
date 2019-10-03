416deb579424d588e28ad40235ef40a1608fe40a
Gerald Pharin <gerald.pharin@andela.com>
2019-10-03 22:35:53 +0000

    Set pharingee as the default repo name

--- scripts/utilities/getGhRepo/index.js
+++ scripts/utilities/getGhRepo/index.js
@@ -4,7 +4,7 @@ const getGhRepo = async (username, repo) => {
   const gh = new GitHub({
     token: process.env.GITHUB_TOKEN,
   });
-  return gh.getRepo(username || 'bbc', repo || 'psammead');
+  return gh.getRepo(username || 'pharingee', repo || 'psammead');
 };
 
 module.exports = getGhRepo;
